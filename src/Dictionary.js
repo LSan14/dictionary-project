94036bd6c2d33647140c36318bc7add7210a0085
Lizeth Sandoval Valladares <liz.sandoval09@gmail.com>
2022-04-08 17:32:43 -0700

    Added axios.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordchange(event) {
