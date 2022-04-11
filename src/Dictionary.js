dc568fa23e0403c9d8fb03d8089ee7f4f8886e97
Lizeth Sandoval Valladares <liz.sandoval09@gmail.com>
2022-04-11 12:02:01 -0700

    Added files

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,18 @@
-import axios from "axios";
 import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
@@ -25,6 +27,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordchange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
